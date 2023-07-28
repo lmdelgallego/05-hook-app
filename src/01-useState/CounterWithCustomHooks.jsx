e83519eb7b9ba2f8170c9f2bac965f25712dc9e3
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2023-07-28 13:44:53 -0500

    ✨feature: useCustomHook

---
+++ src/01-useState/CounterWithCustomHooks.jsx
@@ -0,0 +1,17 @@
+import { useCounter } from "../hooks/useCounter"
+
+export const CounterWithCustomHooks = () => {
+
+
+  const { counter } = useCounter();
+
+  return (
+    <>
+      <h1>Counter With Custom Hooks: {counter}</h1>
+      <hr />
+      <button className="btn btn-primary">+1</button>
+      <button className="btn btn-primary">Reset</button>
+      <button className="btn btn-primary">-1</button>
+    </>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { CounterApp } from './01-useState/CounterApp'
+import { CounterWithCustomHooks } from './01-useState/CounterWithCustomHooks'
+// import { CounterApp } from './01-useState/CounterApp'
 // import HooksApp from './HooksApp'
 
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CounterApp />
+    <CounterWithCustomHooks/>
   </React.StrictMode>,
 )
