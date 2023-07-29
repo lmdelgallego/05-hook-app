f78791c9c343dc16da9c1a2cbf52503fef76a833
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2023-07-29 10:34:44 -0500

    ✨feature: Implementar funcionalidad de Reset

--- src/02-useEffect/FormWithCustomHook.jsx
+++ src/02-useEffect/FormWithCustomHook.jsx
@@ -1,10 +1,10 @@
 import { useEffect } from "react"
 import { useForm } from "../hooks/useForm"
-import { Message } from "./Message"
 
 export const FormWithCustomHook = () => {
 
-  const { formState, onInputChange, email, username, password } = useForm({
+  // Can you use react-hook-form library
+  const { formState,  email, username, password, onInputChange, onResetForm } = useForm({
     username: '',
     email: '',
     password: ''
@@ -16,10 +16,20 @@ export const FormWithCustomHook = () => {
     <>
       <h1>Simple Form</h1>
       <hr />
-      <input type="text" className="form-control mb-2" placeholder="Username" name='username' value={username} onChange={onInputChange}/>
-      <input type="email" className="form-control mb-2" placeholder="luismi@google.com" name='email' value={email} onChange={onInputChange}/>
-      <input type="password" className="form-control" name='password' value={password} onChange={onInputChange}/>
-      { username === 'lmdelgallego' && <Message />}
+      <div className="row mb-2">
+        <div className="col">
+          <input type="text" className="form-control mb-2" placeholder="Username" name='username' value={username} onChange={onInputChange}/>
+          <input type="email" className="form-control mb-2" placeholder="luismi@google.com" name='email' value={email} onChange={onInputChange}/>
+          <input type="password" className="form-control" name='password' value={password} onChange={onInputChange}/>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col">
+          <button className="btn btn-primary" onClick={ onResetForm }>Reset</button>
+        </div>
+      </div>
+
     </>
   )
 }
