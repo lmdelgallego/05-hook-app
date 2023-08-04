ee458e01461ce18358e0b543470a300f4fc09c7e
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2023-08-04 11:57:54 -0500

    ✨feature: crear useTodo hook

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -1,8 +1,7 @@
-import { useEffect, useReducer } from 'react'
-import { todoReducer, TodoList, TodoAdd } from './'
+import { TodoList, TodoAdd } from './'
+import { useTodo } from '../hooks'
 
-const initialState = [
-]
+const initialState = []
 
 const init = () => {
   return JSON.parse(localStorage.getItem('todos')) || []
@@ -10,55 +9,22 @@ const init = () => {
 
 export const TodoApp = () => {
 
-  const [todos, dispatch] = useReducer(todoReducer, initialState, init)
-
-
-  useEffect(() => {
-    localStorage.setItem('todos', JSON.stringify(todos))
-  }, [todos])
-  
-
-  const handleNewTodo = (todo) => {
-    dispatch({
-      type: '[TODO] Add Todo',
-      payload: todo,
-    })
-  }
-
-  const handleDelete = (todoId) => {
-    dispatch({
-      type: '[TODO] Remove Todo',
-      payload: todoId,
-    })
-  }
-
-  const handleToggleTodo = (todoId) => {
-    dispatch({
-      type: '[TODO] Toggle Todo',
-      payload: todoId,
-    })
-  }
-
+  const { todos, handleNewTodo, handleDelete, handleToggleTodo } = useTodo()
 
   return (
     <>
       <h1>TodoApp {todos.length}, <small>pendientes: 2</small> </h1>
-
       <hr />
-
       <div className="row">
         <div className="col-7">
           <TodoList todos={todos} onDeleteTodo={handleDelete} onToggleTodo={handleToggleTodo}/>
         </div>
-
         <div className="col-5">
           <h4>Add TODO</h4>
           <hr />
           <TodoAdd onNewTodo={handleNewTodo} />
         </div>
       </div>
-
-      
     </>
   )
 }
---
+++ src/hooks/useTodo.js
@@ -0,0 +1,48 @@
+import { useEffect, useReducer } from 'react'
+import { todoReducer } from '../08-useReducer'
+
+const initialState = []
+
+const init = () => {
+  return JSON.parse(localStorage.getItem('todos')) || []
+}
+
+export const useTodo = () => {
+
+  const [todos, dispatch] = useReducer(todoReducer, initialState, init)
+
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos))
+  }, [todos])
+
+
+
+  const handleNewTodo = (todo) => {
+    dispatch({
+      type: '[TODO] Add Todo',
+      payload: todo,
+    })
+  }
+
+  const handleDelete = (todoId) => {
+    dispatch({
+      type: '[TODO] Remove Todo',
+      payload: todoId,
+    })
+  }
+
+  const handleToggleTodo = (todoId) => {
+    dispatch({
+      type: '[TODO] Toggle Todo',
+      payload: todoId,
+    })
+  }
+
+  return {
+    todos,
+    handleNewTodo,
+    handleDelete,
+    handleToggleTodo,
+  }
+}
