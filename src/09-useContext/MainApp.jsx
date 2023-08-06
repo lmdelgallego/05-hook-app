7980d1e8c04b0d9331c91d45db6c9d10d832882e
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2023-08-06 12:53:53 -0500

    ✨feature: Configurar Router en React

--- src/09-useContext/MainApp.jsx
+++ src/09-useContext/MainApp.jsx
@@ -1,8 +1,19 @@
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { HomePage, AboutPage, LoginPage } from './';
+
 export const MainApp = () => {
   return (
     <>
       <h1>MainApp</h1>
       <hr />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="login" element={<LoginPage />} />
+        <Route path="about" element={<AboutPage />} />
+
+        {/* <Route path="/*" element={<LoginPage />} /> */}
+        <Route path="/*" element={<Navigate to="/about" />} />
+      </Routes>
     </>
   )
 }
