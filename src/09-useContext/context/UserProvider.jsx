bc1741fafe59dbf82ba10ffb2b17b8a24908024b
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2023-08-08 16:04:08 -0500

    ✨feature: useContext

--- src/09-useContext/context/UserProvider.jsx
+++ src/09-useContext/context/UserProvider.jsx
@@ -1,14 +1,18 @@
+import { useState } from "react"
 import { UserContext } from "./UserContext"
 
-const user = {
-  id: 1,
-  name: 'Luis Miguel Del Gallego',
-  email: 'lmdelgallego@gmail.com'
-}
+// const user = {
+//   id: 1,
+//   name: 'Luis Miguel Del Gallego',
+//   email: 'lmdelgallego@gmail.com'
+// }
 
 export const UserProvider = ({ children }) => {
+
+  const [user, setUser] = useState();
+
   return (
-    <UserContext.Provider value={{ hola: 'Mundo',  user }}>
+    <UserContext.Provider value={{ user, setUser }}>
       {children}
     </UserContext.Provider>
   )
