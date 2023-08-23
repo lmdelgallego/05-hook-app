6464376453cc5b0a0a17c91d6ea40f56c231616f
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2023-08-23 14:38:51 -0500

    ✨feature: evaluating useFetch

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -1,12 +1,11 @@
-import React from 'react';
 import { useCounter, useFetch } from '../hooks';
 import { IsLoading } from './IsLoading';
 import { Quote } from './Quote';
 
 export const MultipleCustomHooks = () => {
   const { counter, increment } = useCounter(1);
-  const { data, isLoading, hasError } = useFetch(
-    `https://api.quotable.io/quotes?page=${counter}&limit=10`
+  const { data, isLoading } = useFetch(
+    `https://api.quotable.io/quotes?page=${counter}&limit=10`,
   );
 
   // if (isLoading) {
@@ -27,7 +26,11 @@ export const MultipleCustomHooks = () => {
           <Quote id={_id} author={author} content={content} key={_id} />
         ))
       )}
-      <button className="btn btn-primary" disabled={isLoading} onClick={() => increment()}>
+      <button
+        className="btn btn-primary"
+        disabled={isLoading}
+        onClick={() => increment()}
+      >
         Next Quote
       </button>
     </>
--- tests/03-example/MultipleCustomHooks.test.jsx
+++ tests/03-example/MultipleCustomHooks.test.jsx
@@ -1,15 +1,55 @@
 import { render, screen } from '@testing-library/react';
 import { MultipleCustomHooks } from '../../src/03-examples/MultipleCustomHooks';
+import { useFetch } from '../../src/hooks/useFetch';
+import { useCounter } from '../../src/hooks/useCounter';
+
+jest.mock('../../src/hooks/useFetch');
+jest.mock('../../src/hooks/useCounter');
 
 describe('MultipleCustomHooks', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+    useCounter.mockReturnValue({
+      counter: 1,
+      increment: jest.fn(),
+    });
+  });
+
   test('should render default component', () => {
+    useFetch.mockReturnValue({
+      data: null,
+      isLoading: true,
+      hasError: null,
+    });
     render(<MultipleCustomHooks />);
     expect(screen.getByText('Quotes'));
     expect(screen.getByText('Loading...'));
 
     const nextButton = screen.getByRole('button');
     expect(nextButton.disabled).toBeTruthy();
+  });
 
-    screen.debug();
+  test('should be show a quote', () => {
+    useFetch.mockReturnValue({
+      data: [{ _id: 1, author: 'Luis Miguel', content: 'Hola Mundo' }],
+      isLoading: false,
+      hasError: null,
+    });
+    render(<MultipleCustomHooks />);
+    const nextButton = screen.getByRole('button');
+    expect(screen.getByText('Hola Mundo')).toBeTruthy();
+    expect(nextButton.disabled).toBeFalsy();
+  });
+
+  test('should be call increment', () => {
+    useFetch.mockReturnValue({
+      data: [{ _id: 1, author: 'Luis Miguel', content: 'Hola Mundo' }],
+      isLoading: false,
+      hasError: null,
+    });
+    render(<MultipleCustomHooks />);
+    const nextButton = screen.getByRole('button');
+    nextButton.click();
+    expect(useCounter().increment).toHaveBeenCalled();
   });
 });
